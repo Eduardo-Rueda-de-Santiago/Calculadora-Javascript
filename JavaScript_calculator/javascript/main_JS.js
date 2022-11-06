e2d3fe77f8273bda2912682fb1a9092ea82c96ff
Eduardo Rueda de Santiago <115037378+Eduardo-Rueda-de-Santiago@users.noreply.github.com>
2022-11-06 16:34:59 +0100

    Menos bugs, pero falta comentarios

--- JavaScript_calculator/javascript/main_JS.js
+++ JavaScript_calculator/javascript/main_JS.js
@@ -1,12 +1,13 @@
-
     //Variables globales
    
         //Utilizadas para guardar los operandos.
         var operando_1 = '';
+        var simbol = '';
         var operando_2 = '';
 
         //Utilizada para alamacenar lo que debe enseñar la pantalla de la calculadora.
-        var oper_display = '';
+        var oper_1_display = '';
+        var oper_2_display = '';
             
         //Utilizadas para almacenar el resultado y el últimoresultado (para usarlo directamente en operaciones futuras).
         var result = '';
@@ -20,100 +21,74 @@
         //Utilizado para dar mensajes de error.
         var error_msg = 0;
 
-    //Funciones (en este programa, como siempre necesita la interacción del usuario no puede haber código fuera de la calculadora).
 
-        //Función para añadir los nuevos números.
-        function get_new_number (num_new){
-
-        //If para que si empìezas a poner un nuevo número después de operar borre lo que había antes.
-        if (error_msg === 1){
-
-            document.getElementById('calculadora_screen_res').innerHTML = '';  
-            error_msg = 0;
-
-        }
-            //Añade el nuevo número al primer operando.
-            //El oper display se usa para luego añadir el simbolo de la operación (ver función operate).
-            if (oper_state === 0){
-
-                operando_1 += num_new;
-
-                oper_display = operando_1;
-
-                document.getElementById('calculadora_screen_oper').innerHTML = oper_display;
-
-            }
-                      
-            //Añade el nuevo número al segundo operando.
+        function nuevo_num (num_new){
+            
             if (oper_state === 1){
 
-                operando_2 += num_new;
-            
-                document.getElementById('calculadora_screen_oper').innerHTML = oper_display + operando_2;
+                oper_state = 2;
 
             }
-            
+
+                //Añade el nuevo número al primer operando.
+                //El oper display se usa para luego añadir el simbolo de la operación (ver función operate).
+                if (oper_state === 0){
+    
+                    operando_1 += num_new;
+    
+                    oper_1_display = operando_1;
+    
+                    document.getElementById('calculadora_screen_oper').innerHTML = oper_1_display;
+    
+                }
+                          
+                //Añade el nuevo número al segundo operando.
+                if (oper_state === 2){
+    
+                    operando_2 += num_new;
+
+                    oper_2_display = operando_2;
+                
+                    document.getElementById('calculadora_screen_oper').innerHTML = oper_1_display + simbol + oper_2_display;
+    
+                }
+                
         }
 
-        //Función para cambiar del primer al seguno operando.
-        function check_oper (caso_oper){
+        function check_oper (tipo_oper){
+        
 
-           
-            if (operando_1 === '' && result != ''){
+            //Algo que chequee si metiste un menos, y asignar simbolo negativo.
 
-                //Si el operando 1 está vacio asignarle el valor de reult. De esta forma se evita el error NaN
-                operando_1 = result;
 
-                //Imprimir el operando 1, que sino se queda un espacio en blanco, luego la operación, y luego el operando 2.
-                document.getElementById('calculadora_screen_oper').innerHTML = operando_1;
+            if ((operando_1 === '') && (tipo_oper === '-')){
 
-                //Cambiar el display para que en la función 'getnumber' se actualice (utilizaba oper_display en vez de operando_1).
-                oper_display = operando_1;
+                operando_1 = '-';
 
-            }
-            //Si la calculadora ya estaba recibiendo el segundo operando, es decir, ya estaba lista para operar, y aun así se quiere hacer otra operación.
-            if (oper_state === 1){
+                oper_1_display = operando_1;
 
-                //Opera primero, para obtener el resultado.
-                operar ();
+                document.getElementById('calculadora_screen_oper').innerHTML = oper_1_display;
 
-                //El operando_1 será igual al resultado.
-                operando_1 = result;
+            }   
+            else if  (operando_1 !== ''){
 
-                /*IMPORTANTE: lo que se cambia aquí es el oper_display (lo que el usuario ve) en vez de el operando_1, tus números
-                De esta forma el usuario ve la operación desglosada y los operandos y el result no reciben números raros.
-                Además como la calculadora seguirá en 'oper_state = 1' no se modificará el oper_display. Esto solo se hace en al función
-                'get_new_number' cuando oper_state = 0.
-                */
-                oper_display = '(' + oper_display + operando_2 + ')';
+                    oper_state = 1;
 
-                //Resetea el operando 2 para que puedas usarlo de nuevo.
-                operando_2 = '';
+                cambiar_operador (tipo_oper);
 
-            }
-            
-            //Elige la operación.
-            cambiar_operador (caso_oper);
+            }           
 
         }
-
-
+  
         function print_res (){
             
             //Que hacer si no hay operando_2
-            if (oper_state === 0){
+            if ( (oper_state === 0) || (oper_state === 1)){
                 
                 result = parseFloat(operando_1);
                 end_operation ();
 
             }
-
-            else if (operando_2 === ''){
-
-                document.getElementById('calculadora_screen_oper').innerHTML = 'Pendejo';
-
-            }
-
             //Que hacer si los dos operandos existen
             else{
 
@@ -124,84 +99,68 @@
 
 
         }
-        
-        //Función para limpiar los resultados.
-        function all_clear (){
 
-            //Borra todo y reinicia la calculadora.
-            document.getElementById('calculadora_screen_res').innerHTML = ' ';
-            document.getElementById('calculadora_screen_oper').innerHTML = ' ';
-            operando_1 = '';
-            operando_2 = '';
-            result = '';
-        
-            oper_display = '';
-            oper_state = 0;
-
-        }
-
-
-        //Cambiar que tipo de operación se esta haciendo y representarlo en el display.
         function cambiar_operador (tipo_oper){
             
-            //Si tras el anterior check operando 1 sigue siendo '' envia mensaje de error.
-            if (operando_1 === ''){
-                
-                document.getElementById('calculadora_screen_res').innerHTML = "Introduzca un primer operando válido.";
-                error_msg = 1;
-            }
-
-            //Si operando 1 existe:
-            else{
+            if (oper_state === 1){
 
                 switch (tipo_oper){
 
                     case '+':
 
-                        //Cambia quien recibe números de operando 1 a operando 2
-                        oper_state = 1;
-
                         //Cambia el tipo de operación en función del simbolo clicado
                         oper_type = 0;
 
                         //Añade el símbolo de la operación al display
-                        oper_display += '+';
+                        simbol = '+';
 
                         //Imprime el display de nuevo
-                        document.getElementById('calculadora_screen_oper').innerHTML = oper_display;
-    
+                        document.getElementById('calculadora_screen_oper').innerHTML = oper_1_display + simbol;
+
                     break;
                     case '-':
-    
-                        oper_state = 1;
+
+                        //Cambia el tipo de operación en función del simbolo clicado
                         oper_type = 1;
-                        oper_display += '-';
-                        document.getElementById('calculadora_screen_oper').innerHTML = oper_display;
-    
+
+                        //Añade el símbolo de la operación al display
+                        simbol = '-';
+
+                        //Imprime el display de nuevo
+                        document.getElementById('calculadora_screen_oper').innerHTML = oper_1_display + simbol;
+
                     break;
                     case '*':
-    
-                        oper_state = 1;
+
+                        //Cambia el tipo de operación en función del simbolo clicado
                         oper_type = 2;
-                        oper_display += '*';
-                        document.getElementById('calculadora_screen_oper').innerHTML = oper_display;
-    
+
+                        //Añade el símbolo de la operación al display
+                        simbol = '*';
+
+                        //Imprime el display de nuevo
+                        document.getElementById('calculadora_screen_oper').innerHTML = oper_1_display + simbol;
+
                     break;
                     case '/':
-    
-                        oper_state = 1;
+
+                        //Cambia el tipo de operación en función del simbolo clicado
                         oper_type = 3;
-                        oper_display += '/';
-                        document.getElementById('calculadora_screen_oper').innerHTML = oper_display;
-    
+
+                        //Añade el símbolo de la operación al display
+                        simbol = '/';
+
+                        //Imprime el display de nuevo
+                        document.getElementById('calculadora_screen_oper').innerHTML = oper_1_display + simbol;
+
                     break;
-    
+
                 }
+        
             }
             
         }
 
-        //Función que decide como operar en función del tipo de operación introducida.
         function operar (){
 
             switch (oper_type){
@@ -229,6 +188,7 @@
             }
                 
         }
+
         function end_operation (){
 
             //Vaciar la casilla e imprimir el nuevo resultado.
@@ -240,14 +200,50 @@
             operando_2 = '';
         
             //Reiniciar lo que se enseña
-            oper_display = '';
+            oper_1_display = '';
+            oper_2_display = '';
 
             //Reiniciar el estado (los números introducidos pertenecen al operando_1 de nuevo).
             oper_state = 0;
 
         }
 
+        function anter_res (){
+
+            if (oper_state === 0){
+                
+                operando_1 = result;
+
+                oper_1_display = operando_1;
+
+                document.getElementById('calculadora_screen_oper').innerHTML = oper_1_display;
+
+            }
+            if (oper_state === 1){
+                
+                operando_2 = result;
+
+                oper_2_display = operando_2;
+
+                document.getElementById('calculadora_screen_oper').innerHTML = oper_1_display + simbol + oper_2_display;
+
+                oper_state = 2;
+            }
 
 
+        }
 
+        function all_clear (){
 
+            //Borra todo y reinicia la calculadora.
+            document.getElementById('calculadora_screen_res').innerHTML = ' ';
+            document.getElementById('calculadora_screen_oper').innerHTML = ' ';
+            operando_1 = '';
+            operando_2 = '';
+            result = '';
+        
+            oper_1_display = '';
+            oper_2_display = '';
+            oper_state = 0;
+
+        }
